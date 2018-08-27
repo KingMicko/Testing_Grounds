2617e4fa655da1003a16992f694887b6ddbacb56
MICKS-PC\Mick <mickoconnor@live.co.uk>
2018-08-28 00:52:19 +0100

    Configuring the gun

--- Source/Testing_Grounds/Player/FirstPersonCharacter.cpp
+++ Source/Testing_Grounds/Player/FirstPersonCharacter.cpp
@@ -77,11 +77,11 @@ void AFirstPersonCharacter::BeginPlay()
 	// Call the base class  
 	Super::BeginPlay();
 
+	/// Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	if (!GunBlueprint) { return; }
+	
 
-	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
-	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
-	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	
 
 	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
 	if (bUsingMotionControllers)
@@ -108,8 +108,16 @@ void AFirstPersonCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
+	/// Attach gun to player mesh
+	if (Gun == NULL)
+	{
+		Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+		Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+		Gun->AnimInstance = Mesh1P->GetAnimInstance();
+	}
+
 	// Bind fire event
-	//PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFirstPersonCharacter::OnFire);
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
 
 	// Enable touchscreen input
 	EnableTouchscreenMovement(PlayerInputComponent);
--- Source/Testing_Grounds/Weapons/Gun.cpp
+++ Source/Testing_Grounds/Weapons/Gun.cpp
@@ -21,7 +21,7 @@ AGun::AGun()
 
 	FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
 	FP_MuzzleLocation->SetupAttachment(FP_Gun);
-	FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
+	FP_MuzzleLocation->SetRelativeLocation(FVector(0.0f, 55.0f, 11.0f));
 }
 
 // Called when the game starts or when spawned
